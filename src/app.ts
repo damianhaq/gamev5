7660a058262cc2507de1174bc8ac8b21970dba03
damianhaq <damianhaq@gmail.com>
2023-03-05 21:44:18 +0100

    fix5

--- src/app.ts
+++ src/app.ts
@@ -26,7 +26,7 @@ export const c: CanvasRenderingContext2D = canvas.getContext("2d");
 c.imageSmoothingEnabled = false;
 
 export const spriteSheet = new Image();
-spriteSheet.src = "../../assets/0x72_DungeonTilesetII_v1.4.png";
+spriteSheet.src = "../assets/0x72_DungeonTilesetII_v1.4.png";
 
 canvas.style.width = dimensions.canvas.sw;
 canvas.style.height = dimensions.canvas.sh;
